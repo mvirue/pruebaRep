717a00e9feaa1e26ee84a7bc4dc9a79073a0eb10
Mario Virue Guerrero <mariovirueguerrero@uma.es>
2026-03-13 19:45:52 +0100

    Constructor arreglado

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -16,9 +16,10 @@ import java.text.SimpleDateFormat;
  * month, and year values cannot be changed.
  */
 public class Date {
-    this.day = dia;
-    this.month = mes;
-    this.year = año;
+    private int fieldAddedByCollaborator;
+  private final int day;
+  private final int month;
+  private final int year;
 
   /**
    * Retrieves the day component of this date.
@@ -55,10 +56,10 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  public Date(int dia, int mes, int año) {
+      this.day = dia;
+      this.month = mes;
+      this.year = año;
   }
 
   /**
